f1a2e1f41796cf1fe9252add6223a97f2fe458d5
Yevhenii Parasochka <singularis314@gmail.com>
2022-11-24 21:42:28 +0100

    DAy 26 continue

--- day_27/main.py
+++ day_27/main.py
@@ -1,13 +1,25 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("test")
 window.minsize(width=500, height=300)
 
 #label
-label = tkinter.Label(text="Test label", font=("Arial", 24, "bold"))
-label.pack(side="left")
+label = Label(text="Test label", font=("Arial", 24, "bold"))
+label.pack()
 
+def button_clicked():
+    print("Clicked")
+    button = input.get()
+    label.config(text=button)
+    return button
+
+
+button = Button(text="Click", command=button_clicked)
+button.pack()
+
+input = Entry()
+input.pack()
 
 
 window.mainloop()
