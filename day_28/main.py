9c627cc146164285bc3ea1b535f3ebf58dc62463
singularis <singularis314@gmail.com>
2022-11-26 21:30:52 +0100

    DAy 28 starT

---
+++ day_28/main.py
@@ -0,0 +1,41 @@
+from tkinter import *
+
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+background_img = PhotoImage(file="./tomato.png")
+canvas.create_image(100, 112, image=background_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+timer_label.grid(column=1, row=0)
+
+checkmark_label = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 35, "bold"))
+checkmark_label.grid(column=1, row=3)
+
+button_start = Button(text="Start")
+button_start.grid(column=0, row=2)
+
+button_stop = Button(text="Reset")
+button_stop.grid(column=2, row=2)
+
+window.mainloop()
