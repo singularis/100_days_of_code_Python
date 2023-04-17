b513a23b2492df8645d140c48ebc3078c1f2feb7
parasochka <singularis314@gmail.com>
2023-04-17 21:24:19 +0200

    Day 35 part 3

--- day_35/main.py
+++ day_35/main.py
@@ -1,6 +1,7 @@
 import secret
 import requests
 import pprint
+ALERT_LEVEL = 700
 parameters = {
     "lat": secret.my_lat,
     "lon": secret.my_lon,
@@ -11,7 +12,14 @@ parameters = {
 
 response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=parameters)
 response.raise_for_status()
-weather_data = response.json()
+weather_data = response.json()["list"]
 pprint.pprint(weather_data)
-print(weather_data)
+for weather_slice in weather_data:
+    weather_point = weather_slice["weather"][0]
+    weather_code = weather_point["id"]
+    weather_time = weather_slice["dt_txt"]
+    weather_description = weather_point["description"]
+    if weather_code < ALERT_LEVEL:
+        print(weather_time)
+
 
