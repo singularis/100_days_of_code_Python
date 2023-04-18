af62a2b4ed8e32098032d7ef119bc1bdb5cb0669
parasochka <singularis314@gmail.com>
2023-04-18 22:45:19 +0200

    day 35 end

--- day_35/main.py
+++ day_35/main.py
@@ -1,6 +1,8 @@
 import secret
 import requests
 import pprint
+import smtplib
+
 ALERT_LEVEL = 700
 parameters = {
     "lat": secret.my_lat,
@@ -10,16 +12,22 @@ parameters = {
     "appid": secret.weather_api_key,
 }
 
+
+def send_mail(receiver, text):
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=secret.email, password=secret.password)
+        connection.sendmail(from_addr=secret.email, to_addrs=receiver, msg=f"Subject:Dante alert!\n\n{text}")
+
+
 response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params=parameters)
 response.raise_for_status()
 weather_data = response.json()["list"]
-pprint.pprint(weather_data)
 for weather_slice in weather_data:
     weather_point = weather_slice["weather"][0]
     weather_code = weather_point["id"]
     weather_time = weather_slice["dt_txt"]
     weather_description = weather_point["description"]
     if weather_code < ALERT_LEVEL:
-        print(weather_time)
-
-
+        send_mail(secret.receiver, f"Expected {weather_description} at {weather_time}.")
+        break
